ae90c38e8dde637c8c100f7ba711f18a8be4ed59
flyingyizi <flyingyizi@gmail.com>
2023-05-07 21:48:58 +0800

    1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-// #![no_std]
-#![cfg_attr(not(test), no_std)]
-#![cfg_attr(test, feature(test))]
+#![no_std]
+// #![cfg_attr(not(test), no_std)]
+// #![cfg_attr(test, feature(test))]
 
 mod style;
 
